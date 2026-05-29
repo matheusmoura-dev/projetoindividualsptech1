c3189513dd65f299dfd6e414bf2657f7c2b9fae3
matheusmoura-dev <matheus.lmoura@sptech.school>
2026-05-29 00:00:58 -0300

    fix: configura conexão com banco de dados na VM

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -4,7 +4,8 @@ var conexao = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 module.exports = conexao;
